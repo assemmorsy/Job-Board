135ad5ee55bbaff564dc8af4f5d395157f66203f
Assem Morsy <assem_EN30894826@f-eng.tanta.edu.eg>
2021-01-30 20:10:05 +0200

    make initial model of job

--- job/models.py
+++ job/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+JOBTYPE = [( "FT", "full time") , ("PT" , "part time")]
 # Create your models here.
+
+class category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+
+class Job(models.Model):
+    title = models.CharField( max_length=50)
+    job_type =models.CharField(max_length=2 , choices=JOBTYPE)
+    description = models.TextField()
+    published_at=models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(  default=1)
+    salary = models.IntegerField(default= 0)
+    experiance = models.IntegerField(default= 1)
+    
+    category = models.ForeignKey(category, default= 1,  on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
